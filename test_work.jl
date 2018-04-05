0d0aab38866e3e3e9017749041f5ec68f1f20315
Dillon Daudert <dillongdaudert@gmail.com>
2018-04-05 16:48:55 -0400

    Add split_work 50% and 1% tests, passing

--- test_work.jl
+++ test_work.jl
@@ -10,5 +10,22 @@ include("work.jl")
     @test_throws DomainError split_work(testwork, -0.5)
     @test_throws DomainError split_work(testwork2, 1.63)
 
+    @testset "split_work 50% test" begin
+        u1, v1 = split_work(testwork, 0.5)
+        u2, v2 = split_work(testwork2, 0.5)
+        @test u1.units == 5
+        @test v1.units == 5
+        @test u2.units == 6
+        @test v2.units == 5
+    end
+
+    @testset "split_work 1% test" begin
+        u1, v1 = split_work(testwork, 0.01)
+        u2, v2 = split_work(testwork2, 0.01)
+        @test u1.units == 1
+        @test v1.units == 9
+        @test u2.units == 1
+        @test v2.units == 10
+    end
 
 end
