81abfa2cdb53fa825d06424fffea29714cabcec3
Dillon Daudert <dillongdaudert@gmail.com>
2018-04-05 16:56:55 -0400

    Fix bug where split_work would not give unit v a minimum of 1 unit

--- work.jl
+++ work.jl
@@ -16,8 +16,11 @@ function split_work(work::WorkUnit, p::T where T<:AbstractFloat)
 
     # check that valid percentage
     !(0 < p < 1) && (@printf("p must be: 0 < p < 1, got %g\n", p); throw(DomainError()))
+
+    # set the rounding mode to up or down depending on p
+    mode = p ≤ .5 ? RoundUp : RoundDown
     
-    u_units = ceil(typeof(work.units), work.units * p)
+    u_units = round(typeof(work.units), work.units * p, mode)
     v_units = work.units - u_units
 
     return WorkUnit(u_units, work.unitcost), WorkUnit(v_units, work.unitcost)
