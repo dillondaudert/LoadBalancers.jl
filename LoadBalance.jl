ff01bbdd94444736efd442f67192900dabce9188
Dillon Daudert <dillongdaudert@gmail.com>
2018-04-13 15:49:30 -0400

    Consolidate RP and SB strategies into LoadBalance.jl; test with driver.jl

--- LoadBalance.jl
+++ LoadBalance.jl
@@ -7,6 +7,6 @@ export parallel_lb, WorkUnit
 include("utils.jl")
 include("worker_utils.jl")
 include("random_poll.jl")
-#include("sched_based.jl")
+include("sched_based.jl")
 
 end
--- driver.jl
+++ driver.jl
@@ -19,3 +19,14 @@ S = Tᵧ/Tₚ
 E = S/P
 
 @printf("Walltime: %4.2f\nOverhead: %4.2f\nSpeedup: %4.2f\nEfficiency: %4.2f\n", Tₚ, Tₒ, S, E)
+
+
+@printf("SB Strategy\n")
+work = WorkUnit(W, Uᵧ)
+Tₚ = parallel_lb_sb(32, work)
+
+Tₒ = P*Tₚ - Tᵧ
+S = Tᵧ/Tₚ
+E = S/P
+
+@printf("Walltime: %4.2f\nOverhead: %4.2f\nSpeedup: %4.2f\nEfficiency: %4.2f\n", Tₚ, Tₒ, S, E)
